328edb6c7c741d417c5d18ca2f77276dda88a87a
Oleksandra <olexandra.momotiuk@gmail.com>
2025-08-13 10:05:01 +0300

    Fix loader

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,26 +1,13 @@
-import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+});
 export function createGallery(images) {
-  if (images.length <= 1) {
-    hideLoader();
-    iziToast.show({
-      title: 'Oops!',
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      backgroundColor: '#fff0f6',
-      titleColor: '#ff4d79',
-      messageColor: '#ff80a0',
-      progressBarColor: '#ffb3c6',
-    });
-    return;
-  }
   const gallery = document.querySelector('.gallery');
   const markup = images.map(image => createImage(image)).join('');
   hideLoader();
   gallery.innerHTML = markup;
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-  });
   lightbox.refresh();
 }
 export function clearGallery() {
@@ -29,13 +16,11 @@ export function clearGallery() {
 }
 export function showLoader() {
   const loader = document.querySelector('.loader');
-  const classes = loader.classList;
-  classes.toggle(`invisible`);
+  loader.style.display = 'block';
 }
 export function hideLoader() {
   const loader = document.querySelector('.loader');
-  const classes = loader.classList;
-  classes.toggle(`invisible`);
+  loader.style.display = 'none';
 }
 function createImage(image) {
   return `<li><div class="image-box">
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -7,11 +6,12 @@ import {
   createGallery,
   clearGallery,
   showLoader,
+  hideLoader,
 } from './js/render-functions';
 
 const refs = {
   form: document.querySelector('.form'),
-    gallery: document.querySelector('.gallery'),
+  gallery: document.querySelector('.gallery'),
 };
 
 refs.form.addEventListener('submit', handleFormSubmit);
@@ -20,22 +20,40 @@ function handleFormSubmit(e) {
   e.preventDefault();
   const userValue = refs.form.elements.searchtext.value;
   if (userValue.trim() === '') {
-   iziToast.show({
-     title: 'Oi!',
-     message:
-       'Please enter a valid search option.',
-     backgroundColor: '#fff0f6',
-     titleColor: '#ff4d79',
-     messageColor: '#ff80a0',
-     progressBarColor: '#ffb3c6',
-   });
+    iziToast.show({
+      title: 'Oi!',
+      message: 'Please enter a valid search option.',
+      backgroundColor: '#fff0f6',
+      titleColor: '#ff4d79',
+      messageColor: '#ff80a0',
+      progressBarColor: '#ffb3c6',
+    });
     e.target.reset();
     return;
   }
   clearGallery();
   showLoader();
   getImagesByQuery(userValue)
-    .then(res => createGallery(res.hits))
-    .catch(err => console.log(err));
-  e.target.reset();
+    .then(res => {
+      if (res.hits.length === 0) {
+        iziToast.show({
+          title: 'Oops!',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          backgroundColor: '#fff0f6',
+          titleColor: '#ff4d79',
+          messageColor: '#ff80a0',
+          progressBarColor: '#ffb3c6',
+        });
+        hideLoader();
+        e.target.reset();
+        return;
+      } else {
+        createGallery(res.hits);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      e.target.reset();
+    });
 }
